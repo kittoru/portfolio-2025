6c9e4b9b2321a786b6ae0eaa86f87bd1459cc648
Kittoru <mama190774@gmail.com>
2024-12-20 16:42:17 +0300

    feat: add new components

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -1,1 +1,5 @@
 export { Header } from './Header/Header';
+export { Hero } from './Hero/Hero';
+export { About } from './About/About';
+export { Projects } from './Projects/Projects';
+export { Footer } from './Footer/Footer';
