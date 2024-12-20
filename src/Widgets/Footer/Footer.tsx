1dc8263f65fd7afd82da11f212fef9f550782474
Kittoru <mama190774@gmail.com>
2024-12-20 16:36:47 +0300

    feat: add Footer

--- src/Widgets/Footer/Footer.tsx
+++ src/Widgets/Footer/Footer.tsx
@@ -2,10 +2,21 @@ import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className="conatiner">
+    <footer className={styles.footer} id='contacts'>
+      <div className="container">
         <div className={styles.inner}>
-          
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <a href="https://github.com/kittoru" target='blank' className={styles.link}>github</a>
+            </li>
+            <li className={styles.item}>
+              <a href="mailto:gnetnevaee@gmail.com" className={styles.link}>gnetnevaee@gmail.com</a>
+            </li>
+            <li className={styles.item}>
+              <a href="https://t.me/kittoruFromHell" target='blank' className={styles.link}>telegram</a>
+            </li>
+          </ul>
+          <span className={styles.text}>Portfolio 2025</span>
         </div>
       </div>
     </footer>
