f5ce03076ba3c6905ad2a6806053793e0f826977
Kittoru <mama190774@gmail.com>
2024-12-21 18:04:42 +0300

    refactor: deleted components and state

--- src/Widgets/Header/Header.tsx
+++ src/Widgets/Header/Header.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-import { Burder, Logo } from '../../Shared';
+import { Logo } from '../../Shared';
 import styles from './Header.module.scss';
 
 export const Header = () => {
-  const [status, getStatus] = useState<boolean>(false);
-  
   return (
     <header className={styles.header}>
       <div className="container">
