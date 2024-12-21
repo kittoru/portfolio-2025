dcf5823a416340a3e839992d597ed6cf16045689
Kittoru <mama190774@gmail.com>
2024-12-21 18:03:12 +0300

    feat: add adptive

--- src/Widgets/Header/Header.tsx
+++ src/Widgets/Header/Header.tsx
@@ -1,7 +1,10 @@
-import { Logo } from '../../Shared';
+import { useState } from 'react';
+import { Burder, Logo } from '../../Shared';
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const [status, getStatus] = useState<boolean>(false);
+  
   return (
     <header className={styles.header}>
       <div className="container">
