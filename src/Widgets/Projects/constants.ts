246fdce5af67b7e81760335fc56233bfa9039226
Kittoru <mama190774@gmail.com>
2024-12-22 17:56:57 +0300

    fix: change description

--- src/Widgets/Projects/constants.ts
+++ src/Widgets/Projects/constants.ts
@@ -25,7 +25,7 @@ export const technologyArr = [
   {
     id: 3,
     name: 'The gifts',
-    description: 'Данный проект был выполнен во время прохождения курса RS School. В ходе работы были реализованы две страницы, на каждой из которых были разработаны механизмы и анимации в соответствие с ТЗ.',
+    description: 'Данный проект был выполнен во время прохождения курса RS School. Проект был разработан в соответствие с ТЗ.',
     img: Gifts,
     technology: ['HTML', 'CSS', 'JS'],
     link: 'https://rolling-scopes-school.github.io/kittoru-JSFE2024Q4/christmas-shop/index.html'
@@ -33,7 +33,7 @@ export const technologyArr = [
   {
     id: 4,
     name: 'Weather',
-    description: 'Данный проект был разработан на React и представляет собой приложения для ознакомления с погодой в определённом городе. Приложение разрабатывалось с целью изучения работы с Redux Toolkit и axios. Данные о погоде берутся из OpenWeatherMap.',
+    description: 'Данный проект был разработан на React. Данные о погоде берутся из OpenWeatherMap.',
     img: Weather,
     technology: ['ReactJS', 'Redux Toolkit', 'FSD', 'axios', 'SCSS', 'Vite'],
     link: 'https://kittoru.github.io/weather-redux/'
@@ -41,7 +41,7 @@ export const technologyArr = [
   {
     id: 5,
     name: 'Todo',
-    description: 'Данный проект был разработан с целью изучения впервую очередь работы с Redux Toolkit и представляет собой обычный ToDo list. В нём реализованы базовые функции: добавить, изменить, изменить статус, удалить.',
+    description: 'Данный проект был разработан на React. В нём реализованы базовые функции CRUD.',
     img: Todo,
     technology: ['ReactJS', 'FSD', 'Redux Toolkit', 'SCSS'],
     link: 'https://kittoru.github.io/Todo-List/'
