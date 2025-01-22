8db7910a63f7412d920bcd62c9c70f691820791f
Kittoru <mama190774@gmail.com>
2025-01-22 22:41:16 +0300

    feat: add animation

--- src/Shared/ui/Item/Item.tsx
+++ src/Shared/ui/Item/Item.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import styles from './Item.module.scss';
 
 type Props = {
@@ -15,20 +16,26 @@ export const Item = ({id, name, description, img, technology, link}: Props) => {
   })
   return ( 
     <li key={id} className={styles.item}>
-      <div className={styles.top}>
+      <motion.div className={styles.top} initial={{ opacity: 0, x: "-100%" }} animate={{ opacity: 1, x: '0%'}} exit={{ opacity: 0, x: "-100%" }} transition={{ duration: 1 }}>
           <h3 className={styles.title}>{name}</h3>
           <p className={styles.description}>{description}</p>
-          <a href={link} target='blank' className={styles.btn}>Перейти</a>
-      </div>
-      <a href={link} target='blank' className={styles.link}>
-         <img src={img} className={styles.img}></img>
-      </a>
-      <div className={styles.bottom}>
-          <h3 className={styles.title}>Использовались</h3>
+          <motion.a href={link} target='blank' className={styles.btn} whileHover={{ scale: 1.1 }} transition={{ duration: 0.2 }}>Перейти</motion.a>
+      </motion.div>
+      <motion.a href={link} target='blank' className={styles.link} 
+        initial={{ scale: 0, opacity: 0}} 
+        animate={{ scale: 1, opacity: 1 }} 
+        transition={{ duration: 1 }}
+        exit={{ scale: 0, opacity: 0 }}>
+         <motion.img src={img} className={styles.img}
+          animate={{ rotate: [15, -15] }} transition={{ duration: 4, ease: 'anticipate', repeat: Infinity, repeatType: "reverse" }}
+         ></motion.img>
+      </motion.a>
+      <motion.div className={styles.bottom} initial={{ opacity: 0, x: "100%" }} animate={{ opacity: 1, x: '0%'}} exit={{ opacity: 0, x: "100%" }} transition={{ duration: 1 }}>
+          <h3 className={styles.title}>Технологии</h3>
             <ul className={styles.list}>
               {list}
             </ul>
-      </div>
+      </motion.div>
     </li>
   );
 }
