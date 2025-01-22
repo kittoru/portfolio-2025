13b4aebbfe4be547d295f72e6393588c0c0f3bc5
Kittoru <mama190774@gmail.com>
2025-01-22 21:06:36 +0300

    fix: change spring on morion animation

--- src/Widgets/Header/Header.tsx
+++ src/Widgets/Header/Header.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react';
 import { Burger, Logo, useScrollLock } from '../../Shared';
 import styles from './Header.module.scss';
-import { animated, useTransition } from '@react-spring/web';
+import { AnimatePresence } from 'motion/react';
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   useScrollLock(isOpen);
-
-  const transitions = useTransition(isOpen, {
-    from: {transform: 'translateX(100%)'},
-    enter: {transform: 'translateX(0%)'},
-    leave: {transform: 'translateX(100%)'},
-    config: {
-      tension: 280, 
-      friction: 60,
-    },
-  });
 
   return (
     <header className={styles.header}>
@@ -34,13 +24,9 @@ export const Header = () => {
             <span></span>
             <span></span>
           </button>
-          {transitions((style, isOpen) => (
-            <>
-            {isOpen? <animated.div style={style} className={styles.mobile}>
-              <Burger close={setIsOpen}/>
-            </animated.div>: <></>}
-          </>
-          ))}
+          <AnimatePresence>
+            {isOpen? <Burger key={1} close={setIsOpen}/>: <></>}
+          </AnimatePresence>
         </div>
       </div>
     </header>
