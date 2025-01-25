5a89bdf14ce4b940c19d0579800bbb48b4338e78
Kittoru <mama190774@gmail.com>
2025-01-26 01:28:31 +0300

    feat: add animation for hover

--- src/Shared/ui/Logo/Logo.tsx
+++ src/Shared/ui/Logo/Logo.tsx
@@ -1,7 +1,10 @@
 import styles from './Logo.module.scss';
+import { motion } from 'motion/react';
 
 export const Logo = () => {
   return <div className={styles.logo}>
-    <a href="#" className={styles.link}>GnetnevaEE</a>
+    <motion.a whileHover={{
+                  fontWeight: 700,
+                  textShadow: '5px 5px 3px rgb(164, 162, 180, 0.5)'}} transition={{ duration: 0.6 }} href="#" className={styles.link}>GnetnevaEE</motion.a>
   </div>
 }
