41650d1e0f77d74077c2ebdb80db1dd2e9b7c5d7
Kittoru <mama190774@gmail.com>
2025-01-26 01:29:44 +0300

    feat: add animation for hover

--- src/Widgets/Footer/Footer.tsx
+++ src/Widgets/Footer/Footer.tsx
@@ -1,20 +1,25 @@
 import styles from './Footer.module.scss';
+import { motion } from 'motion/react';
 
 export const Footer = () => {
+  const val = {
+    whileHover: { fontWeight: 500,textShadow: '5px 5px 3px rgb(164, 162, 180, 0.5)'},
+    transition: { duration: 0.6 }
+  };
   return (
     <footer className={styles.footer} id='contacts'>
       <div className="container">
         <div className={styles.inner}>
           <ul className={styles.list}>
-            <li className={styles.item}>
+            <motion.li variants={val} whileHover={val.whileHover} transition={val.transition} className={styles.item}>
               <a href="https://github.com/kittoru" target='blank' className={styles.link}>github</a>
-            </li>
-            <li className={styles.item}>
+            </motion.li>
+            <motion.li variants={val} whileHover={val.whileHover} transition={val.transition} className={styles.item}>
               <a href="mailto:gnetnevaee@gmail.com" className={styles.link}>gnetnevaee@gmail.com</a>
-            </li>
-            <li className={styles.item}>
+            </motion.li>
+            <motion.li variants={val} whileHover={val.whileHover} transition={val.transition} className={styles.item}>
               <a href="https://t.me/kittoruFromHell" target='blank' className={styles.link}>telegram</a>
-            </li>
+            </motion.li>
           </ul>
           <span className={styles.text}>Portfolio 2025</span>
         </div>
