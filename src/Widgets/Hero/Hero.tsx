f9c4cdb946912fc8160cf1c96358b53ad824ff2d
Kittoru <mama190774@gmail.com>
2025-01-27 13:04:48 +0300

    feat: add animation from text when those initial

--- src/Widgets/Hero/Hero.tsx
+++ src/Widgets/Hero/Hero.tsx
@@ -1,23 +1,22 @@
 import styles from './Hero.module.scss';
-
+import { motion } from 'motion/react';
 
 export const Hero = () => {
-  
   return <section className={styles.hero}>
     <div className="container">
       <div className={styles.inner}>
         <div className={styles.left}>
-          <p className={styles.text}>
+          <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{duration: 1}} className={styles.text}>
           Здравствуйте, <br/>
           я Елизавета Гнетнёва
-        </p>
-        <p className={styles.subtext}>Junior frontend developer</p>
+        </motion.p>
+        <motion.p initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{duration: 1, delay: 1}} className={styles.subtext}>Junior frontend developer</motion.p>
         </div>
-        <div className={styles.right}>
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 2}}  className={styles.right}>
           <div className={`${styles.back} ${styles.circle}`}>
             <div className={`${styles.center} ${styles.circle}`}></div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   </section>
