acff63b1b5c6a361c25fbc1cb8004690cbb2b925
Kittoru <mama190774@gmail.com>
2025-01-27 18:08:53 +0300

    fix: change visible arrowUp

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,14 +1,29 @@
-import styles from './Home.module.scss';
 import { About, Footer, Header, Hero, Projects } from "../../Widgets"
 import { ArrowUp } from '../../Shared';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from "motion/react";
 
+import styles from './Home.module.scss';
 
 export const Home = () => {
+  const [isVisible, setIsVisible] = useState<number>(0);
+  
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    setIsVisible(window.scrollY)
+  }
+
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <ArrowUp />
+        <AnimatePresence>
+          {isVisible > 200? <ArrowUp />: null }
+        </AnimatePresence>
         <Hero />
         <About />
         <Projects />
