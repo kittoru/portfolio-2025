e19553718dafd9524bcaed257efa1cebca5d4c4a
Kittoru <mama190774@gmail.com>
2025-01-27 14:53:15 +0300

    feat: add animation when initial

--- src/Shared/ui/Logo/Logo.tsx
+++ src/Shared/ui/Logo/Logo.tsx
@@ -2,9 +2,9 @@ import styles from './Logo.module.scss';
 import { motion } from 'motion/react';
 
 export const Logo = () => {
-  return <div className={styles.logo}>
+  return <motion.div initial={{opacity: 0, x: '-100%'}} animate={{opacity: 1, x: '0%'}} transition={{duration: 1}} className={styles.logo}>
     <motion.a whileHover={{
                   fontWeight: 700,
                   textShadow: '5px 5px 3px rgb(164, 162, 180, 0.5)'}} transition={{ duration: 0.6 }} href="#" className={styles.link}>GnetnevaEE</motion.a>
-  </div>
+  </motion.div>
 }
