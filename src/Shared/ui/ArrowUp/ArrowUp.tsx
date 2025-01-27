12f42b2bf8f63ca8740e106aa0404203f6881746
Kittoru <mama190774@gmail.com>
2025-01-27 18:09:49 +0300

    feat: add animation initial and exit

--- src/Shared/ui/ArrowUp/ArrowUp.tsx
+++ src/Shared/ui/ArrowUp/ArrowUp.tsx
@@ -1,13 +1,10 @@
-import { motion, useScroll, useTransform } from 'motion/react';
-
+import { motion } from 'motion/react';
 
 import styles from './ArrowUp.module.scss';
 
 export const ArrowUp = () => {
-  const { scrollYProgress } = useScroll();
-  const opacity = useTransform(scrollYProgress, [0.1, 0.3], [0, 1]);
   return ( 
-    <motion.a href='#logo' initial={{opacity: 0}} style={{opacity}} className={styles.btn}>
+    <motion.a href='#logo' initial={{opacity: 0, y: 20}} animate={{opacity:1, y: 0}} exit={{opacity: 0, y: 20}} transition={{duration: 0.5}} className={styles.btn}>
       <div className={styles.arrow}>
         <span className={styles.side}></span>
         <span className={styles.side}></span>
