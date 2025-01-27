06eda2310ccab1256c8261736305a8a830927400
Kittoru <mama190774@gmail.com>
2025-01-27 16:13:34 +0300

    feat: add listener and function for touch on touchscreen devices

--- src/Widgets/Projects/Projects.tsx
+++ src/Widgets/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import styles from './Projects.module.scss';
 import { AnimatePresence, motion } from 'motion/react';
 
 export const Projects = () => {
+  const [touchPosition, setTouchPosition] = useState<number>(0);
   const [indexProject, setIndexProject] = useState<number>(0);
   const list = technologyArr.map(item => {
     return <Item key={item.id} id={item.id} name={item.name} description={item.description} img={item.img} technology={item.technology} link={item.link} />
@@ -37,13 +38,32 @@ export const Projects = () => {
     }
     setIndexProject(list.length - 1);
   };
+
+  const handelTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    const touchDown = e.touches[0].clientX
+    setTouchPosition(touchDown);
+  }
+
+  const handelTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    const touchDown = touchPosition;
+
+    if(touchDown === 0) return;
+
+    const currentTouch = e.touches[0].clientX;
+    const diff = touchDown - currentTouch;
+
+    if (diff > 5) moveNext();
+    if (diff < -5) moveBack();
+
+    setTouchPosition(0)
+  }
   
   return ( 
     <motion.section initial={{opacity: 0}} whileInView={{opacity:1, transition: {duration: 1, delay: 0.5}}} viewport={{once: true}} className={styles.projects} id='projects'>
       <div className="container">
         <div className={styles.inner}>
           <motion.h2 initial={{opacity: 0, y: 20}} whileInView={{opacity:1, y: 0, transition: {duration: 1, delay: 0.7}}} viewport={{once: true}} className={styles.title}>Проекты</motion.h2>
-          <motion.div initial={{opacity: 0, y: 20}} whileInView={{opacity:1, y: 0, transition: {duration: 1, delay: 1}}} viewport={{once: true}}  className={styles.content}>
+          <motion.div initial={{opacity: 0, y: 20}} whileInView={{opacity:1, y: 0, transition: {duration: 1, delay: 1}}} viewport={{once: true}}  className={styles.content} onTouchStart={handelTouchStart} onTouchMove={handelTouchMove}>
             <motion.button whileHover={{ scale: 1.2 }} transition={{duration: 0.5}} className={styles.btn} onClick={moveBack}>
               <span className={styles.back}></span>
               <span className={styles.back}></span>
