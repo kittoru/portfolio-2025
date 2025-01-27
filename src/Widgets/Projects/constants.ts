27d71d79a72c9bbe683824d99f1959e0040a5377
Kittoru <mama190774@gmail.com>
2025-01-27 15:25:17 +0300

    featL: add a new project

--- src/Widgets/Projects/constants.ts
+++ src/Widgets/Projects/constants.ts
@@ -3,35 +3,12 @@ import Coca from '../../Shared/assets/coca.jpg';
 import Gifts from '../../Shared/assets/gifts.jpg';
 import Todo from '../../Shared/assets/todo.jpg';
 import Weather from '../../Shared/assets/weather.jpg';
+import Form from '../../Shared/assets/form.jpg';
 
 
 export const technologyArr = [
   {
     id: 1,
-    name: 'Simple',
-    description: 'Данный проект является обычным одностраничным лендингом с адаптивом.',
-    img: Simple,
-    technology: ['HTML', 'CSS', 'JS'],
-    link: 'https://kittoru.github.io/Simple/'
-  },
-  {
-    id: 2,
-    name: 'Coca',
-    description: 'Данный проект является обычным одностраничным лендингом с адаптивом.',
-    img: Coca,
-    technology: ['HTML', 'CSS', 'JS'],
-    link: 'https://kittoru.github.io/Coca/'
-  },
-  {
-    id: 3,
-    name: 'The gifts',
-    description: 'Данный проект был выполнен во время прохождения курса RS School. Проект был разработан в соответствие с ТЗ.',
-    img: Gifts,
-    technology: ['HTML', 'CSS', 'JS'],
-    link: 'https://rolling-scopes-school.github.io/kittoru-JSFE2024Q4/christmas-shop/index.html'
-  },
-  {
-    id: 4,
     name: 'Weather',
     description: 'Данный проект был разработан на React. Данные о погоде берутся из OpenWeatherMap.',
     img: Weather,
@@ -39,11 +16,43 @@ export const technologyArr = [
     link: 'https://kittoru.github.io/weather-redux/'
   },
   {
-    id: 5,
+    id: 2,
     name: 'Todo',
     description: 'Данный проект был разработан на React. В нём реализованы базовые функции CRUD.',
     img: Todo,
     technology: ['ReactJS', 'FSD', 'Redux Toolkit', 'SCSS'],
     link: 'https://kittoru.github.io/Todo-List/'
+  },
+  {
+    id: 3,
+    name: 'Form',
+    description: 'Формы входа/регистрации и валидация форм реализованы через библиотеку React Hook Form.',
+    img: Form,
+    technology: ['ReactJS', 'TypeScript', 'FSD', 'SCSS', 'React Hook Form', 'Motion', 'Vite'],
+    link: 'https://kittoru.github.io/form/'
+  },
+  {
+    id: 4,
+    name: 'The gifts',
+    description: 'Данный проект был выполнен во время прохождения курса RS School. Проект был разработан в соответствие с ТЗ.',
+    img: Gifts,
+    technology: ['HTML', 'CSS', 'JS'],
+    link: 'https://rolling-scopes-school.github.io/kittoru-JSFE2024Q4/christmas-shop/index.html'
+  },
+  {
+    id: 5,
+    name: 'Simple',
+    description: 'Данный проект является обычным одностраничным лендингом с адаптивом.',
+    img: Simple,
+    technology: ['HTML', 'CSS', 'JS'],
+    link: 'https://kittoru.github.io/Simple/'
+  },
+  {
+    id: 6,
+    name: 'Coca',
+    description: 'Данный проект является обычным одностраничным лендингом с адаптивом.',
+    img: Coca,
+    technology: ['HTML', 'CSS', 'JS'],
+    link: 'https://kittoru.github.io/Coca/'
   },
 ]
