0250b12898817c2bbdec5fbcb0ed07b25df2ff3e
Kittoru <mama190774@gmail.com>
2025-01-27 17:38:25 +0300

    feat: add id

--- src/Shared/ui/Logo/Logo.tsx
+++ src/Shared/ui/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import { motion } from 'motion/react';
 
 export const Logo = () => {
   return <motion.div initial={{opacity: 0, x: '-100%'}} animate={{opacity: 1, x: '0%'}} transition={{duration: 1}} className={styles.logo}>
-    <motion.a whileHover={{
+    <motion.a id='logo' whileHover={{
                   fontWeight: 700,
                   textShadow: '5px 5px 3px rgb(164, 162, 180, 0.5)'}} transition={{ duration: 0.6 }} href="#" className={styles.link}>GnetnevaEE</motion.a>
   </motion.div>
