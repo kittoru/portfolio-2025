0f9a6f9811f45b91e3025a448394bb277b2d9d2c
Kittoru <mama190774@gmail.com>
2025-01-27 17:40:14 +0300

    feat: add export arrowUp

--- src/Shared/index.ts
+++ src/Shared/index.ts
@@ -1,6 +1,7 @@
 export { Logo } from './ui/Logo/Logo';
 export { Item } from './ui/Item/Item';
 export { Burger } from './ui/Burger/Burger';
+export { ArrowUp } from './ui/ArrowUp/ArrowUp';
 
 export * from './lib/useScrollLock';
 export * from './lib/useWindowDimensions';
